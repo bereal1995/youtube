0557117684c5ed09e2d442ff6a9548a8fbe0be54
HH <gygud98@naver.com>
2021-02-25 02:00:55 +0900

    빌드오류 수정

--- src/view/Containers/WatchContainer/index.js
+++ src/view/Containers/WatchContainer/index.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from 'react';
+import React, {useEffect} from 'react';
 import styled from "styled-components";
 import qs from "qs";
 import {videoActions} from "../../../redux/ActionCreators";
